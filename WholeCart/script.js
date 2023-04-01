17da9018f21e2c618b12aec6ddb52d7a00015fdc
Carson J <84282420+CarsonIndex@users.noreply.github.com>
2023-04-01 11:58:09 -0500

    Create script.js

---
+++ WholeCart/script.js
@@ -0,0 +1,16 @@
+document.getElementById("IDbox").addEventListener("click", getAPI);
+
+function openHomePage() {
+    window.location.href = 'https:/';
+}
+
+function getAPI() {
+    document.getElementById("resulttext").innerHTML = ""
+    fetch("localhost:8000/WholeCart")
+        .then(response => response.text())
+        .then(data => myFunc(data));
+}
+
+function myFunc(Result) {
+    document.getElementById("resulttext").innerHTML = Result
+}
