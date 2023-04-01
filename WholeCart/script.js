b11ad3382bdacd30ad49fdbb9cdc8c1389694415
Carson J <84282420+CarsonIndex@users.noreply.github.com>
2023-04-01 12:18:06 -0500

    Update script.js

--- WholeCart/script.js
+++ WholeCart/script.js
@@ -1,15 +1,7 @@
-document.getElementById("IDbox").addEventListener("click", getAPI);
-
-function openHomePage() {
-    window.location.href = 'https:/';
-}
-
-function getAPI() {
-    document.getElementById("resulttext").innerHTML = ""
-    fetch("https://huener.github.io/hackathon-spring23/WholeCart/")
-        .then(response => response.text())
-        .then(data => myFunc(data));
-}
+document.getElementById("resulttext").innerHTML = ""
+fetch("https://huener.github.io/hackathon-spring23/WholeCart/")
+    .then(response => response.text())
+    .then(data => myFunc(data));
 
 function myFunc(Result) {
     document.getElementById("resulttext").innerHTML = Result
